61092d1950471d5f1551d55d4d2c5e73ea1e3238
Pavan Gowda T S <115703185+pavants777@users.noreply.github.com>
2024-01-16 03:07:22 +0530

    Update map.dart

--- DartLan/map.dart
+++ DartLan/map.dart
@@ -1,1 +1,18 @@
 
+void main()
+{
+  Map<int,int> mp = {     // key : value 
+     1 : 2,
+     2 : 3,
+     3 : 4,
+     4 : 5,
+     5 : 6,
+     7 : 8,
+     8 : 9,
+     9 : 0,
+     0 : 1,
+  };
+
+  final list = mp.entries.where((e)=>e.key%2==0).map((e)=>e.key).toList()..sort();
+  print(list);
+}
